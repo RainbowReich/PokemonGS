a0bc4328272b7eab6c7917af81c85907d6876738
RainbowReich <rainbowryke@gmail.com>
2015-12-12 10:50:20 -0600

    Add structure for editing and viewing pokedex data

---
+++ ruby_gs/lib/ruby_gs/structs/pokedex_data.rb
@@ -0,0 +1,33 @@
+
+
+module RubyGS
+
+  FOUND = 1
+  NOT_FOUND = 0
+
+  class PokedexArray < BinData::Array
+    
+	def [] i
+	  return @element_list[real_index(i)].snapshot 
+	end
+
+	def []= i, v
+      @element_list[real_index(i)].assign v
+	end 
+
+    private
+
+		def real_index n
+		  return n if n % 8 == 0
+		  ((n / 8).floor+2)*8 - (n % 8)
+		end
+
+  end
+
+  class PokedexData < BinData::Record
+    endian :big
+    pokedex_array :species, :type => :bit1, :initial_length => 251 
+    array :unknown, :type => :bit1, :initial_length => 5
+  end
+
+end
